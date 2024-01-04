d04009bad87043a2ea50aaa6f375081ee32019c9
Mike Salvatore <mike.s.salvatore@gmail.com>
2024-01-04 08:50:09 -0500

    Populate vulture_allowlist.py

---
+++ vulture_allowlist.py
@@ -0,0 +1,48 @@
+from monkeytoolbox import (
+    InterruptableThreadMixin,
+    PeriodicCaller,
+    ThreadSafeIterator,
+    append_bytes,
+    apply_filters,
+    create_secure_directory,
+    del_key,
+    expand_path,
+    get_all_regular_files_in_directory,
+    get_binary_io_sha256_hash,
+    get_hardware_id,
+    get_my_ip_addresses,
+    get_text_file_contents,
+    insecure_generate_random_string,
+    interruptible_function,
+    interruptible_iter,
+    make_fileobj_copy,
+    open_new_securely_permissioned_file,
+    port_is_used,
+    queue_to_list,
+    request_cache,
+    secure_generate_random_string,
+)
+
+InterruptableThreadMixin
+PeriodicCaller
+PeriodicCaller.stop
+ThreadSafeIterator
+append_bytes
+apply_filters
+create_secure_directory
+del_key
+expand_path
+get_all_regular_files_in_directory
+get_binary_io_sha256_hash
+get_hardware_id
+get_my_ip_addresses
+get_text_file_contents
+insecure_generate_random_string
+interruptible_function
+interruptible_iter
+make_fileobj_copy
+open_new_securely_permissioned_file
+port_is_used
+queue_to_list
+request_cache
+secure_generate_random_string
